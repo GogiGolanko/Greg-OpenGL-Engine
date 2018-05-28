6211a76fdf5adfdf3675bf79584b89bfdcb3301b
Greg Golanko <grzegorz.golanko@gmail.com>
2018-05-28 16:22:07 +0200

    Inicjalizacja GLEW

--- src/Application.cpp
+++ src/Application.cpp
@@ -5,11 +5,68 @@
 // Copyright   : Copyright c by Greg
 // Description : OpenGL Engine
 //============================================================================
+#include <GL/glew.h>
+#include <GL/gl.h>
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 using namespace std;
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    //glewExperimental = GL_TRUE;
+    GLenum err = glewInit();
+	if (GLEW_OK != err)
+	{
+	  /* Problem: glewInit failed, something is seriously wrong. */
+	  cout << "Error: " << glewGetErrorString(err) << endl;
+
+	}
+
+	cout << "Status: Using GLEW: " << glewGetString(GLEW_VERSION) << endl
+		 << "Version of GLFW: " << glfwGetVersionString() << endl
+		 << "Company: " << glGetString(GL_VENDOR) << endl
+		 << "Name of the renderer: " << glGetString(GL_RENDERER) << endl
+		 << "Version of release: " << glGetString(GL_VERSION) << endl
+		 << "Shading language:" << glGetString(GL_SHADING_LANGUAGE_VERSION) << endl;
+
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f( 0.0f,  0.5f);
+        glVertex2f( 0.5f, -0.5f);
+        glEnd();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
