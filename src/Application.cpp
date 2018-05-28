6285cdb7a9a67213782291afa5b52bc7d6f9061f
Greg Golanko <grzegorz.golanko@gmail.com>
2018-05-28 17:34:55 +0200

    Vertex Attrib Pointer

--- src/Application.cpp
+++ src/Application.cpp
@@ -59,6 +59,11 @@ int main(void)
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
 	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 
+	glVertexAttribIPointer(0, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
