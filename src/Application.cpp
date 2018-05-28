7d2da3b7142d27309e34a490aecedf5003929c1c
Greg Golanko <grzegorz.golanko@gmail.com>
2018-05-28 14:01:49 +0200

    Inicjujący commit

---
+++ src/Application.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Greg-OpenGL-Engine.cpp
+// Author      : Greg
+// Version     : 1.0
+// Copyright   : Copyright c by Greg
+// Description : OpenGL Engine
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
